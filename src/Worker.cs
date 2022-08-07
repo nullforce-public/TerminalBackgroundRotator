f1b436381410c3875b69a4ed10dc2e3701e986cd
nullforce <glenngit@nullforce.com>
2022-08-06 20:30:05 -0500

    Update to .NET 6 and update NuGet packages to latest

--- src/Worker.cs
+++ src/Worker.cs
@@ -9,110 +9,109 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace TerminalBackgroundRotator
+namespace TerminalBackgroundRotator;
+
+public class Worker : BackgroundService
 {
-    public class Worker : BackgroundService
+    private readonly ILogger<Worker> _logger;
+    private readonly IConfiguration _config;
+    private string _filename;
+    private string _originalFilename;
+    private string _profileGuid;
+    private string _wallpaperDirectory;
+    private int _wallpaperIntervalInSeconds = 300;
+
+    public Worker(ILogger<Worker> logger, IConfiguration config)
     {
-        private readonly ILogger<Worker> _logger;
-        private readonly IConfiguration _config;
-        private string _filename;
-        private string _originalFilename;
-        private string _profileGuid;
-        private string _wallpaperDirectory;
-        private int _wallpaperIntervalInSeconds = 300;
+        _logger = logger;
+        _config = config;
+    }
 
-        public Worker(ILogger<Worker> logger, IConfiguration config)
+    public override Task StartAsync(CancellationToken cancellationToken)
+    {
+        _filename = Environment.ExpandEnvironmentVariables(_config.GetValue<string>("Terminal:ProfilePath"));
+        _profileGuid = _config.GetValue<string>("Terminal:ProfileGuid");
+        _wallpaperDirectory = Environment.ExpandEnvironmentVariables(_config.GetValue<string>("Terminal:WallpaperDirectory"));
+        _wallpaperIntervalInSeconds = _config.GetValue<int>("Terminal:WallpaperIntervalInSeconds", _wallpaperIntervalInSeconds);
+
+        // Backup terminal profile
+        var timestamp = DateTime.Now.ToString("yyyyMMddhhmmss");
+        _originalFilename = $"{_filename}.bak{timestamp}";
+        File.Copy(_filename, _originalFilename);
+
+        return base.StartAsync(cancellationToken);
+    }
+
+    public override Task StopAsync(CancellationToken cancellationToken)
+    {
+        // Restore original terminal profile
+        File.Copy(_originalFilename, _filename, overwrite: true);
+
+        return base.StopAsync(cancellationToken);
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        string backgroundImage = null;
+
+        while (!stoppingToken.IsCancellationRequested)
         {
-            _logger = logger;
-            _config = config;
-        }
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
 
-        public override Task StartAsync(CancellationToken cancellationToken)
-        {
-            _filename = Environment.ExpandEnvironmentVariables(_config.GetValue<string>("Terminal:ProfilePath"));
-            _profileGuid = _config.GetValue<string>("Terminal:ProfileGuid");
-            _wallpaperDirectory = Environment.ExpandEnvironmentVariables(_config.GetValue<string>("Terminal:WallpaperDirectory"));
-            _wallpaperIntervalInSeconds = _config.GetValue<int>("Terminal:WallpaperIntervalInSeconds", _wallpaperIntervalInSeconds);
-
-            // Backup terminal profile
-            var timestamp = DateTime.Now.ToString("yyyyMMddhhmmss");
-            _originalFilename = $"{_filename}.bak{timestamp}";
-            File.Copy(_filename, _originalFilename);
-
-            return base.StartAsync(cancellationToken);
-        }
-
-        public override Task StopAsync(CancellationToken cancellationToken)
-        {
-            // Restore original terminal profile
-            File.Copy(_originalFilename, _filename, overwrite: true);
-
-            return base.StopAsync(cancellationToken);
-        }
-
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-        {
-            string backgroundImage = null;
-
-            while (!stoppingToken.IsCancellationRequested)
+            try
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                // Read the terminal profiles
+                var json = File.ReadAllText(_filename);
 
-                try
+                // Get a background image from the provided location
+                var images = Directory.EnumerateFiles(_wallpaperDirectory);
+
+                if (backgroundImage == null)
                 {
-                    // Read the terminal profiles
-                    var json = File.ReadAllText(_filename);
+                    backgroundImage = images.FirstOrDefault();
+                }
+                else
+                {
+                    // Find the next background image after the current one
+                    bool found = false;
+                    string nextImage = null;
 
-                    // Get a background image from the provided location
-                    var images = Directory.EnumerateFiles(_wallpaperDirectory);
-
-                    if (backgroundImage == null)
+                    foreach (string image in images)
                     {
-                        backgroundImage = images.FirstOrDefault();
-                    }
-                    else
-                    {
-                        // Find the next background image after the current one
-                        bool found = false;
-                        string nextImage = null;
-
-                        foreach (string image in images)
+                        if (found)
                         {
-                            if (found)
-                            {
-                                nextImage = image;
-                                break;
-                            }
-
-                            if (image == backgroundImage)
-                            {
-                                found = true;
-                            }
+                            nextImage = image;
+                            break;
                         }
 
-                        backgroundImage = nextImage ?? images.FirstOrDefault() ?? backgroundImage;
+                        if (image == backgroundImage)
+                        {
+                            found = true;
+                        }
                     }
 
-                    // Replace the background image for the given profile
-                    dynamic jobject = JsonConvert.DeserializeObject(json);
-                    var profile = ((IEnumerable<dynamic>)jobject.profiles["list"]).Where(d => d.guid == _profileGuid).SingleOrDefault();
-
-                    if (profile != null)
-                    {
-                        profile.backgroundImage = backgroundImage;
-                    }
-
-                    // Write the profiles back out
-                    var output = JsonConvert.SerializeObject(jobject, Formatting.Indented);
-                    File.WriteAllText(_filename, output);
+                    backgroundImage = nextImage ?? images.FirstOrDefault() ?? backgroundImage;
                 }
-                catch (Exception e)
+
+                // Replace the background image for the given profile
+                dynamic jobject = JsonConvert.DeserializeObject(json);
+                var profile = ((IEnumerable<dynamic>)jobject.profiles["list"]).Where(d => d.guid == _profileGuid).SingleOrDefault();
+
+                if (profile != null)
                 {
-                    _logger.LogCritical(e, "An exception was caught!");
+                    profile.backgroundImage = backgroundImage;
                 }
 
-                await Task.Delay(_wallpaperIntervalInSeconds * 1000, stoppingToken);
+                // Write the profiles back out
+                var output = JsonConvert.SerializeObject(jobject, Formatting.Indented);
+                File.WriteAllText(_filename, output);
             }
+            catch (Exception e)
+            {
+                _logger.LogCritical(e, "An exception was caught!");
+            }
+
+            await Task.Delay(_wallpaperIntervalInSeconds * 1000, stoppingToken);
         }
     }
 }
